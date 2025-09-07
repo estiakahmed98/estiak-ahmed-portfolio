f34386b8487a48e15c10246d4081b860ab8d9827
estiakahmed98 <estiakahmed898@gmail.com>
2025-09-07 12:04:02 +0600

    Updated Hero Section

--- components/hero.tsx
+++ components/hero.tsx
@@ -13,7 +13,7 @@ const SparklesCore = dynamic(
 
 export default function Hero() {
   const [title, setTitle] = useState("Jr. Software Engineer");
-  const titles = ["Jr. Software Engineer", "UI/UX Designer"];
+  const titles = ["Jr. Software Engineer", "Frontend Developer"];
   const [titleIndex, setTitleIndex] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
 
